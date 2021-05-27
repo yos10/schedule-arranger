17dfe3c23301a4590f608ebc225740a3fe082a59
yos10 <61726023+yos10@users.noreply.github.com>
2021-05-27 15:35:04 +0900

    【サービス開発5】出欠の表示と更新

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ User.sync().then(() => {
   Schedule.sync();
   Comment.belongsTo(User, { foreignKey: 'userId' });
   Comment.sync();
+  Availability.belongsTo(User, {foreignKey: 'userId'});
   Candidate.sync().then(() => {
     Availability.belongsTo(Candidate, { foreignKey: 'candidateId' });
     Availability.sync();
@@ -63,6 +64,7 @@ var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var logoutRouter = require('./routes/logout');
 var schedulesRouter = require('./routes/schedules');
+var availabilitiesRouter = require('./routes/availabilities');
 
 var app = express();
 app.use(helmet());
@@ -91,6 +93,7 @@ app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
 app.use('/schedules', schedulesRouter);
+app.use('/schedules', availabilitiesRouter);
 
 app.get(
   '/auth/github',
