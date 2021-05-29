f402d3bc5ee97dd9a826d68501825ad23845377d
yos10 <61726023+yos10@users.noreply.github.com>
2021-05-29 17:40:03 +0900

    Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,8 +1,31 @@
 'use strict';
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(
-  'postgres://postgres:postgres@db/schedule_arranger'
-);
+
+const dialectOptions = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+};
+
+const sequelize = process.env.DATABASE_URL ?
+  // 本番環境
+  new Sequelize(
+    process.env.DATABASE_URL,
+    {
+      logging: false,
+      dialectOptions,
+      ssl: true,
+    }
+  )
+  :
+  // 開発環境
+  new Sequelize(
+    'postgres://postgres:postgres@db/schedule_arranger',
+    {
+      logging: false
+    }
+  );
 
 module.exports = {
   sequelize,
